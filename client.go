cf62c159f55a25014edc9e7ae712b661240408a8
olebedev <ole6edev@gmail.com>
2017-11-15 10:27:45 +0500

    Add API throttling into the client

--- client.go
+++ client.go
@@ -7,8 +7,30 @@ import (
 	httptransport "github.com/go-openapi/runtime/client"
 	"github.com/go-openapi/strfmt"
 	"github.com/olebedev/go-tgbot/client"
+	"golang.org/x/time/rate"
 )
 
+// Add wrapper for API throttling
+type wrapper struct {
+	*rate.Limiter
+	*httptransport.Runtime
+	context.Context
+}
+
+// Submit wraps httpclient.Submit for throttling
+func (w *wrapper) Submit(op *runtime.ClientOperation) (interface{}, error) {
+	if op.Context != nil {
+		w.Wait(op.Context)
+	} else if w.Context != nil {
+		w.Wait(w.Context)
+	} else {
+		w.Wait(context.Background())
+	}
+
+	return w.Runtime.Submit(op)
+}
+
+// NewClient returns new plain API client
 func NewClient(ctx context.Context, token string) *client.TelegramBot {
 	transport := httptransport.New("api.telegram.org", "/", []string{"https"})
 	if ctx == nil {
@@ -19,16 +41,9 @@ func NewClient(ctx context.Context, token string) *client.TelegramBot {
 		return r.SetPathParam("token", token)
 	})
 
-	// transport.Consumers[runtime.JSONMime] = runtime.ConsumerFunc(func(reader io.Reader, data interface{}) error {
-	// 	dec := json.NewDecoder(reader)
-	// 	dec.UseNumber() // preserve number formats
-	// 	return dec.Decode(data)
-	// })
-	//
-	// transport.Producers[runtime.JSONMime] = runtime.ProducerFunc(func(writer io.Writer, data interface{}) error {
-	// 	enc := json.NewEncoder(writer)
-	// 	return enc.Encode(data)
-	// })
-
-	return client.New(transport, nil)
+	return client.New(&wrapper{
+		Limiter: rate.NewLimiter(30, 1),
+		Runtime: transport,
+		Context: ctx,
+	}, nil)
 }
